f9a7cb4d51d7035c2d60833f7ef0a23dbfcd3a03
Harshit keshari <65784019+Harshit107@users.noreply.github.com>
2023-07-10 23:43:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const userRouter = require('./src/router/UserRouter');
 
 const PORT = process.env.PORT || 5000;
 
-// Initialize Express
 const app = express();
 app.use(express.json());
 
@@ -12,6 +11,5 @@ app.use("/users",userRouter);
 
 app.get("/checkserver", (req, res) => res.send("<h1>Hey Developer! Server is working fine, Go aHead!"));
 
-// Start server
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
